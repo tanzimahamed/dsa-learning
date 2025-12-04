f9d890fb2f9bd0cb6f00c7518b86444df72be697
Tanzim Ahamed <tanzim.ahamed.bd@gmail.com>
2025-12-05 01:04:30 +0600

    added new file

---
+++ Queue/Queue.py
@@ -0,0 +1,64 @@
+
+# Without class 
+queue = []
+
+# Enqueue
+queue.append('A')
+queue.append('B')
+queue.append('C')
+print("Queue: ", queue)
+
+# Dequeue
+element = queue.pop(0)
+print("Dequeue: ", element)
+
+# Peek
+frontElement = queue[0]
+print("Peek: ", frontElement)
+
+# isEmpty
+isEmpty = not bool(queue)
+print("isEmpty: ", isEmpty)
+
+# Size
+print("Size: ", len(queue))
+
+
+
+
+
+
+
+
+# creating a class 
+
+class Queue:
+    def __init__(self):
+        self.queue = []
+
+    def enqueue(self, value):
+        self.queue.append(value)
+
+    def dequeue(self):
+        if not self.is_empty():
+            return self.queue.pop(0)
+        return "Queue is empty"
+
+    def front(self):
+        if not self.is_empty():
+            return self.queue[0]
+        return "Queue is empty"
+
+    def is_empty(self):
+        return len(self.queue) == 0
+
+
+# Test
+q = Queue()
+q.enqueue(10)
+q.enqueue(20)
+q.enqueue(30)
+
+print("Front element:", q.front())
+print("Dequeued:", q.dequeue())
+print("Queue now:", q.queue)
