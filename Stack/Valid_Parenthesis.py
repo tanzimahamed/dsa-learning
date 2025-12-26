0dd61703dc09f60f3c714787580373dcd9a7120b
Tanzim Ahamed <tanzim.ahamed.bd@gmail.com>
2025-12-26 21:09:40 +0600

    Add new files

---
+++ Stack/Valid_Parenthesis.py
@@ -0,0 +1,16 @@
+def isValid(s: str) -> bool:
+    stack = []
+    mapping = {')': '(', '}': '{', ']': '['}
+
+    for char in s:
+        if char in mapping:
+            if not stack or stack.pop() != mapping[char]:
+                return False
+        else:
+            stack.append(char)
+
+    return not stack
+print(isValid("()")) # True
+print(isValid("({[]})")) # True
+print(isValid("(]")) # False
+print(isValid("())(")) # False
